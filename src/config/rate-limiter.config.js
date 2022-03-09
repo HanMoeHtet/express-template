5f0c84c79cce42501941782fdc80e9e9d66693d7
HanMoeHtet <hapo23lert@gmail.com>
2022-03-09 20:22:37 +0630

    updated: rate limitter middleware

--- src/config/rate-limiter.config.js
+++ src/config/rate-limiter.config.js
@@ -1,6 +1,7 @@
 import { RateLimiterMemory } from 'rate-limiter-flexible';
+import { ENV } from './env.config';
 
-export const REQUESTS_PER_SECOND_PER_IP = 10;
+export const REQUESTS_PER_SECOND_PER_IP = ENV !== 'test' ? 20 : 10;
 
 export const rateLimiterByIp = new RateLimiterMemory({
   points: REQUESTS_PER_SECOND_PER_IP,
