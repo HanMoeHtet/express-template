e250c7057e2fc30b2c558b6e8e8df5338e24762c
HanMoeHtet <hapo23lert@gmail.com>
2022-03-09 20:08:47 +0630

    updated: jest config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   moduleFileExtensions: ['js', 'json'],
   rootDir: 'test',
   testRegex: '.*\\.test\\.js$',
-  collectCoverageFrom: ['**/*.js'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
   setupFiles: [path.join(__dirname, './src/bootstrap.js')],
